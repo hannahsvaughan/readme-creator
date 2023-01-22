c5a7d830123eff6d059596aa7834157be2d3a4d1
hannahsvaughan <hannahvaughan421@gmail.com>
2023-01-22 09:06:32 -0500

    added functions in index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
@@ -137,13 +138,35 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fileName, data, error => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve ({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
 
 // TODO: Create a function to initialize app
 function init() {
-
-
-
+    inquirer.prompt(questions)
+        .then(answers => {
+            console.log(answers);
+            return generateMarkdown(answers);
+        })
+        .then(pageMarkdown => {
+            writeToFile('./dist/README.md', pageMarkdown);
+            console.log('README.md created!')
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
 // Function call to initialize app
